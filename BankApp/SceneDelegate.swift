77074f1d5347379d69b795b9a297da9453f9a136
Nadezhda Nikitenko <karina_kovaleva_ios@icloud.com>
2024-05-27 11:51:22 +0300

    INITIAL setup

---
+++ BankApp/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  BankApp
+//
+//  Created by Karina Kovaleva on 27.05.24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+
---
+++ BankApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Karina Kovaleva on 27.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBlue
+    }
+}
